c45f34276f8b7780a309555759096ff3f94bd939
Jayaram Nair <jayaramsivaramannair@gmail.com>
2022-01-10 20:52:47 -0500

    Added styling to create shell of the calculator with styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import './App.css';
+import Button from './components/Button'
 
 function App() {
+
+  const buttonKeys = ['AC', '/', 'x', '7', '8', '9', '-', '4', '5', '6', '+', '1', '2', '3', '0', '.', '=']
   return (
     <div className="App">
-      Hello, World!!!
+      <div className="display">0</div>
+      <div className="buttons">
+        {
+          buttonKeys.map((button, idx) => {
+            return <Button key={idx} />
+          })
+        }
+      </div>
     </div>
   );
 }
---
+++ src/components/Button.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Button = () => {
+  return (
+    <div className ="button">
+      But
+    </div>
+  )
+
+
+}
+
+export default Button;
