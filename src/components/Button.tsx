7e72767e94bee0356dd847a74eda8f64b5b4b7f9
Jayaram Nair <jayaramsivaramannair@gmail.com>
2022-01-12 06:15:15 -0500

    Added a clickHandler function to recognize the buttons clicked

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,12 +3,21 @@ import React from 'react';
 interface buttonProps {
   button : string
   key : number
+  operations: string[]
+  setResult: React.Dispatch<React.SetStateAction<number>>
+  setOperations: React.Dispatch<React.SetStateAction<string[]>>
 }
 
 //Button is a functional component which deals with buttonProps
-const Button: React.FC<buttonProps> = ({button}) => {
+const Button: React.FC<buttonProps> = ({button, setResult, setOperations, operations}) => {
+
+  const clickHandlerFunction = (event: React.MouseEvent<HTMLDivElement, MouseEvent>)=> {
+    event.preventDefault();
+    console.log(event.currentTarget.firstChild?.textContent)
+    setOperations([...operations, button])
+  }
   return (
-    <div className ={`button`}>
+    <div className ={`button button-${button}`} onClick={clickHandlerFunction}>
       {button}
     </div>
   )
--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Button from './Button';
 const Calculator = () => {
   const buttonKeys = ['AC', '/', 'x', '7', '8', '9', '-', '4', '5', '6', '+', '1', '2', '3', '0', '.', '=']
 
+  const [result, setResult] = useState<number>(0);
+  const [operations, setOperations] = useState<string []>([]);
+
   const renderButtonList = (): JSX.Element[] => {
     return buttonKeys.map((button, idx) => {
-      return <Button key={idx} button={button}/>
+      return <Button key={idx} button={button} setResult={setResult} setOperations={setOperations} operations={operations}/>
     })
+  }
+
+  const displayOperations = (): string => {
+    return operations.join(" ");
   }
   
   return (
     <div className="shell">
-      <div className="display">0</div>
+      <div className="operations">{displayOperations()}</div>
+      <div className="display">{result}</div>
       <div className="buttons">
         {renderButtonList()}
       </div>
